f3cbe1990af4715e9f597cf6d4e58bf356ef7bdd
Sylwek <sylwero98#o2.pl>
2021-09-27 21:36:34 +0200

    Initial Commit

---
+++ src/main/java/pl/pollub/sppd/SppdApplication.java
@@ -0,0 +1,13 @@
+package pl.pollub.sppd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SppdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SppdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pollub/sppd/SppdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pollub.sppd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SppdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c29b55b26b2e853fc614a9d61542551ce705d266
Bartistero <bartek114@autograf.pl>
2021-12-06 18:49:07 +0100

    Added statistic API method.
    1.getCountPerson() -> (only Student, Lecturer permission). This method return amount peron in application.
    
    2. getCountThesis() -> this methos return amount of thesis broken down by status.

--- src/main/java/pl/pollub/sppd/model/repository/ThesisTitleRepository.java
+++ src/main/java/pl/pollub/sppd/model/repository/ThesisTitleRepository.java
@@ -3,6 +3,9 @@ package pl.pollub.sppd.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.pollub.sppd.model.ThesisTitle.ThesisTitle;
 import pl.pollub.sppd.model.address.Street;
+import pl.pollub.sppd.model.thesisStatus.ThesisStatus;
 
 public interface ThesisTitleRepository extends JpaRepository<ThesisTitle, Long> {
+
+    Long countByThesisStatus(ThesisStatus thesisStatus);
 }
